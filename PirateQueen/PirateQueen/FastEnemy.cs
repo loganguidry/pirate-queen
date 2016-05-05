f0f5f829b11f8c9eba56f4ee99e41892dc93e715
Andrew Harding (RIT Student) <axh1130@ad.rit.edu>
2016-05-05 15:10:58 -0400

    Added skeleton classes for fast and heavy enemy

---
+++ PirateQueen/PirateQueen/FastEnemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PirateQueen
+{
+    // reps fast enemy, child of Enemy
+    class FastEnemy:Enemy
+    {
+        public FastEnemy(Texture2D sprt, Texture2D walk, Vector2 pos, int randomSeed, string kind) : base(sprt, walk, pos, randomSeed, kind)
+        {
+
+        }
+    }
+}
---
+++ PirateQueen/PirateQueen/HeavyEnemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PirateQueen
+{
+    // reps heavy enemy, child of Enemy
+    class HeavyEnemy:Enemy
+    {
+        public HeavyEnemy(Texture2D sprt, Texture2D walk, Vector2 pos, int randomSeed, string kind):base(sprt,walk,pos,randomSeed,kind)
+        {
+
+        }
+    }
+}
